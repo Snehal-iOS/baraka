be958186b917f5ba78c7456df032b69b2dd385dc
Yomly-snehal <snehal@yomly.com>
2025-06-23 15:55:17 +0400

    Initial Commit

---
+++ BarakaTests/BarakaTests.swift
@@ -0,0 +1,17 @@
+//
+//  BarakaTests.swift
+//  BarakaTests
+//
+//  Created by Snehal on 23/06/2025.
+//
+
+import Testing
+@testable import Baraka
+
+struct BarakaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
